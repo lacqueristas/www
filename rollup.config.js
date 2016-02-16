f3e1aa58cd998ac48d6f033ff586cc58e2f79f14
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-02-16 13:08:06 -0800

    Being smarter about rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
 import babel from "rollup-plugin-babel"
+import nodeResolve from "rollup-plugin-node-resolve"
+import commonJS from "rollup-plugin-commonjs"
 
 export default {
   entry: "./client/index.js",
   dest: "./tmp/client/index.js",
-  format: "iife",
   plugins: [
     babel({
       exclude: "node_modules/**"
+    }),
+    nodeResolve({
+      jsnext: true
+    }),
+    commonJS({
+      include: "node_modules/**"
     })
   ]
 }
