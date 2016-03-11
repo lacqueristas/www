28d106c594a33bb5afc987d91240bfc8bf8bc8b6
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-03-11 09:27:42 -0800

    Writing the first test

---
+++ spec/client/sdk/activities/list/indexSpec.js
@@ -0,0 +1,7 @@
+import list from "~/client/sdk/activities/list"
+
+describe("client/sdk/activities/list", () => {
+  it("contains spec with an expectation", () => {
+    expect(list()).toEqual(jasmine.any(Object))
+  })
+})
