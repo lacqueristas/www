8994cbaa30934c0d83bde74fed700a62ef97d6d5
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-03-13 16:41:27 -0700

    Don't need to test this right now

--- client/application/presenter/view/index.js
+++ client/application/presenter/view/index.js
@@ -2,8 +2,6 @@ import {p} from "@cycle/dom"
 import core from "../core"
 
 export default (state) => {
-  console.log('From view:')
-  console.log(state)
   return core(
     p("Hello, world.")
   )
