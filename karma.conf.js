9c96967561f54a4cbb8995830e46a572c959c140
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-03-12 19:46:21 -0800

    Fixing karma stuff

--- karma.conf.js
+++ karma.conf.js
@@ -20,14 +20,14 @@ module.exports = (config) => {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      "client/**/*.js": ["browserify", "coverage"],
+      "client/**/*.js": ["browserify"],
       "spec/**/*Spec.js": ["browserify"]
     },
 
     // test results reporter to use
     // possible values: "dots", "progress"
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ["progress", "coverage"],
+    reporters: ["progress"],
 
     // web server port
     port: 9876,
