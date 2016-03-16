d9b4100cbdd60c81db1e90da41c2194cf28865f9
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-03-16 02:09:50 -0700

    We shouldn't be committing logging to core

--- client/application/model/coerce/index.js
+++ client/application/model/coerce/index.js
@@ -1,9 +1,6 @@
 export default (mapping) => (value, key) => {
-  console.log(key, value)
-  console.log(mapping[key])
   switch (mapping[key]) {
     case "date": {
-      console.log(new Date(value))
       return new Date(value)
     }
     default: {
