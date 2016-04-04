8444108b0826431eae03ecec58e93b6080fab9de
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-04-03 17:24:44 -0700

    Now we can use development + envifiy

--- client/sdk/v1Activities/list/index.js
+++ client/sdk/v1Activities/list/index.js
@@ -1,12 +1,9 @@
-import Dotenv from "dotenv"
 import {join} from "ramda"
 
-Dotenv.load({silent: true})
-
-const PROTOCOL = process.env.API_PROTOCOL
-const HOST = process.env.API_HOST
-const PORT = process.env.API_PORT
-const MEDIATYPE = process.env.API_MEDIATYPE
+const PROTOCOL: string = process.env.API_PROTOCOL || "http"
+const HOST: string = process.env.API_HOST || "localhost"
+const PORT: string = process.env.API_PORT || "8081"
+const MEDIATYPE: string = process.env.API_MEDIATYPE || "application/lacqueristas.api+json; version=1"
 const ACTIVITIES_PATH: string = "v1/activities"
 const ACTIVITIES_LIST_METHOD: string = "GET"
 const ACTIVITIES_LIST_URL: string = `${PROTOCOL}://${HOST}:${PORT}/${ACTIVITIES_PATH}`
