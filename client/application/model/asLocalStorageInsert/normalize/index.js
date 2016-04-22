3be9836bed8550c93e504b753a2fc2acbd37f6ac
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-04-21 20:01:25 -0700

    Further simplifying for the sake of fixing a bug

--- client/application/model/asLocalStorageInsert/normalize/index.js
+++ client/application/model/asLocalStorageInsert/normalize/index.js
@@ -1,16 +1,15 @@
 import {prop} from "ramda"
 import {groupBy} from "ramda"
 import {map} from "ramda"
-import {pipe} from "ramda"
 
 import asRecords from "./asRecords"
 
-export default pipe(
+// @contract("{links, data, included} -> {...} | {}")
+export default (payload) => {
   // {links: Object, data: Array[n], included: Array[n]}
-  prop("data"),
-  // [Object, n]
-  groupBy(prop("type")),
-  // {[type]: Array[n], n}
-  map(asRecords)
-  // {[type]: Object, n}
-)
+  if (payload.data) {
+    return map(asRecords, groupBy(prop("type"), payload.data))
+    // {[type]: Object, n}
+  }
+  return {}
+}
