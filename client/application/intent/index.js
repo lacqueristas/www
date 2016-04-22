37e10e3416a86d4ec2b050a305a58c6b4fa05dc2
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-04-21 18:31:39 -0700

    We don't need this selector

--- client/application/intent/index.js
+++ client/application/intent/index.js
@@ -1,11 +1,9 @@
 import click$ from "./click$"
 import read$ from "./read$"
 import write$ from "./write$"
-import selectResponse$ from "./selectResponse$"
 
 export {
   click$,
   read$,
-  write$,
-  selectResponse$
+  write$
 }
