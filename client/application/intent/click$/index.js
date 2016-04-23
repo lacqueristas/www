e497266ce9511d24d95420433dadcee997738782
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-04-22 21:56:17 -0700

    Totally not used anymore

--- client/application/intent/click$/index.js
+++
@@ -1,1 +0,0 @@
-export default (DOM$) => DOM$.events("click")
--- client/application/intent/index.js
+++ client/application/intent/index.js
@@ -1,9 +1,7 @@
-import click$ from "./click$"
 import read$ from "./read$"
 import write$ from "./write$"
 
 export {
-  click$,
   read$,
   write$
 }
