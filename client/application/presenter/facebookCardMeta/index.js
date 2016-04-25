4f91673f7d3a13156b2e27fe949cb515e37914d8
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-04-25 15:54:33 -0700

    We're going to want to generate these on resource loads

---
+++ client/application/presenter/facebookCardMeta/index.js
@@ -0,0 +1,12 @@
+// <!-- Facebook OpenGraph -->
+// <!-- Facebook: https://developers.facebook.com/docs/sharing/webmasters#markup -->
+// <!-- Open Graph: http://ogp.me/ -->
+// <meta property="fb:app_id" content="123456789">
+// <meta property="og:url" content="https://example.com/page.html">
+// <meta property="og:type" content="website">
+// <meta property="og:title" content="Content Title">
+// <meta property="og:image" content="https://example.com/image.jpg">
+// <meta property="og:description" content="Description Here">
+// <meta property="og:site_name" content="Site Name">
+// <meta property="og:locale" content="en_US">
+// <meta property="article:author" content="">
---
+++ client/application/presenter/pageMeta/index.js
@@ -0,0 +1,3 @@
+// <meta http-equiv="Expires" content="{{expires}}">
+// <meta http-equiv="Pragma" content="{{pragma}}">
+// <meta http-equiv="Cache-Control" content="{{cache-control}}">
