5a6167bf7c421bf915d26a72060e7e03bc993f8a
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-05-07 12:31:44 -0700

    :ram:

--- client/activities/intent/catchActivitiesList$/index.js
+++ client/activities/intent/catchActivitiesList$/index.js
@@ -2,5 +2,7 @@ import {onlyActivitiesListResponse$$} from "~/client/activities/model"
 import {asNative} from "~/client/application/model"
 
 export default (response$) => {
-  return onlyActivitiesListResponse$$(response$).switch().map(asNative)
+  return onlyActivitiesListResponse$$(response$)
+    .switch()
+    .map(asNative)
 }
