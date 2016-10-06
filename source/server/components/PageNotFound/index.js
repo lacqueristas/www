e8f0676f07875f67b42c32a36204fb2d4596b872
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-10-05 21:07:01 -0700

    Freeing ourselves from the shackles of html

--- source/server/components/PageNotFound/index.js
+++ source/server/components/PageNotFound/index.js
@@ -3,9 +3,10 @@ import React from "react"
 import Layout from "../Layout"
 import SectionHeading from "../SectionHeading"
 
-export default function TermsOfService () {
-  return <Layout title="Terms Of Service">
+export default function PageNotFound () {
+  return <Layout title="PageNotFound">
     <article>
+
     </article>
   </Layout>
 }
