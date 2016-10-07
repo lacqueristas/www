a9dc7bbcb25d1b6d771ef86243209b0837314207
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-10-07 09:55:01 -0700

    We'd like to track and utilize the history api so that users browser aren't confused

--- source/client/index.js
+++ source/client/index.js
@@ -1,9 +1,16 @@
 import React from "react"
 import {render} from "react-dom"
 import {Provider} from "react-redux"
+import createHistory from "history/createBrowserHistory"
 
 import {Application} from "../server/components"
 import store from "./store"
+
+const history = createHistory()
+
+history.listen(function locationChange () {
+  return store.dispatch({type: "NAVIGATE", payload: {location}})
+})
 
 render(
   <Provider store={store}>
--- source/client/store/listener/index.js
+++ source/client/store/listener/index.js
@@ -1,14 +1,17 @@
 import urlParse from "url-parse"
+import createHistory from "history/createBrowserHistory"
 
 export const initialState = {
   starting: true,
   navigation: urlParse(location.href, true)
 }
-
+export const history = createHistory()
 export default function listener (state = initialState, signal) {
   switch (signal.type) {
     case "NAVIGATE": {
       const navigation = urlParse(signal.payload.href, true)
+
+      history.push(navigation.pathname)
 
       return {
         ...state,
