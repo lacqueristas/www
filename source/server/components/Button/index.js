ff43ceb9120173505cb08be0d291008a51575e79
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2016-11-20 17:54:57 -0800

    Like the anchor, we will want a call to action button

---
+++ source/server/components/Button/index.js
@@ -0,0 +1,53 @@
+import React, {Component, PropTypes} from "react"
+import {connect} from "react-redux"
+
+import {primary} from "../colors"
+import {neutral} from "../colors"
+
+const styles = {
+  primary: {
+    background: neutral,
+    borderWidth: "2px",
+    borderStyle: "solid",
+    borderColor: primary
+  }
+}
+
+export default connect()(class Button extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    type: PropTypes.string.isRequired,
+    href: PropTypes.string,
+    dispatch: PropTypes.func.isRequired
+  }
+
+  onClick () {
+    const {dispatch} = this.props
+
+    return function trigger (event) {
+      const {target} = event
+      const {href} = target
+
+      event.preventDefault()
+
+      return dispatch({type: "NAVIGATE", payload: {href}})
+    }
+  }
+
+  render () {
+    const {children} = this.props
+    const {type} = this.props
+    const {href} = this.props
+
+    if (href) {
+      return <button style={styles[type]}>
+        {children}
+      </button>
+    }
+
+    return <button>
+      {children}
+    </button>
+  }
+
+})
