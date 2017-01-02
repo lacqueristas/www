81801c4ffc36fbe9494943568561597850408d12
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-01-02 09:23:07 -0800

    Instead of inlining take advantage of eslint's power

--- source/components/LandingPage/index.js
+++ source/components/LandingPage/index.js
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../Layout"
 import Anchor from "../Anchor"
 
+const callToActionStyle = {padding: 25, display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-around"}
+
 export default function LandingPage () {
   return <Layout subtitle="Welcome to Polish">
     <section>
       <img src="/keyhole-llama.png" alt="the cute llama mascot protruding from a hole in the website" />
-      <section data-intent="call to action" style={{padding: 25, display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-around"}}>
+      <section data-intent="call to action" style={callToActionStyle}>
         <Anchor kind="primary" href="/sign-up">
           Join us
         </Anchor>
