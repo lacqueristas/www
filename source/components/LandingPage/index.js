f37caf7a676d3890244e62d491bf30c223b281c7
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-01-02 09:23:41 -0800

    Working on a new landing page

--- source/components/LandingPage/index.js
+++ source/components/LandingPage/index.js
@@ -2,13 +2,17 @@ import React from "react"
 
 import Layout from "../Layout"
 import Anchor from "../Anchor"
+import Heading from "../Heading"
 
 const callToActionStyle = {padding: 25, display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-around"}
 
 export default function LandingPage () {
   return <Layout subtitle="Welcome to Polish">
     <section>
-      <img src="/keyhole-llama.png" alt="the cute llama mascot protruding from a hole in the website" />
+      <Heading kind="page" subtitle="Welcome to Polish">
+        Lacqueristas
+      </Heading>
+      <img src="/llama-sitting.png" alt="the cute llama mascot sitting and looking silly" />
       <section data-intent="call to action" style={callToActionStyle}>
         <Anchor kind="primary" href="/sign-up">
           Join us
