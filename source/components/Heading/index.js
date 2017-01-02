9b965b5b311aff37da45a425b04499b36dafec96
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-01-02 09:22:39 -0800

    Significantly more flexible

--- source/components/Heading/index.js
+++ source/components/Heading/index.js
@@ -1,8 +1,14 @@
 import React, {PureComponent, PropTypes} from "react"
 
+const headings = {
+  page: "h1",
+  section: "h2"
+}
+
 export default class Heading extends PureComponent {
   static propTypes = {
     children: PropTypes.node.isRequired,
+    subtitle: PropTypes.string,
     style: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.string, PropTypes.number])).isRequired,
     kind: PropTypes.oneOf(["page", "section"]).isRequired
   }
@@ -15,21 +21,18 @@ export default class Heading extends PureComponent {
     const {children} = this.props
     const {style} = this.props
     const {kind} = this.props
+    const {subtitle} = this.props
+    const HeadingElement = headings[kind]
 
-    switch (kind) {
-      case "page": {
-        return <h1 style={{...style}}>
-          {children}
-        </h1>
+    return <header>
+      <HeadingElement className={`Heading-${kind}`} style={{...style}}>
+        {children}
+      </HeadingElement>
+      {
+        subtitle && <p className={`Heading-${kind}-subtitle`}>
+          {subtitle}
+        </p>
       }
-      case "section": {
-        return <h2 style={{...style}}>
-          {children}
-        </h2>
-      }
-      default: {
-        throw new Error("Headings must have a type")
-      }
-    }
+    </header>
   }
 }
