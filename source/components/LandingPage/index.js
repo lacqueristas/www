bc1badacb6c414d4290a272f31f5026f124a8395
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-01-08 00:13:33 -0800

    We want to start utilizing a newsletter format

--- source/components/LandingPage/index.js
+++ source/components/LandingPage/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../Layout"
 import Anchor from "../Anchor"
 import Heading from "../Heading"
+import Newsletter from "./Newsletter"
 
 const callToActionStyle = {
   padding: 25,
@@ -14,11 +15,19 @@ const callToActionStyle = {
 
 export default function LandingPage () {
   return <Layout subtitle="Welcome to Polish">
-    <section>
-      <Heading kind="page" subtitle="Welcome to Polish">
-        Lacqueristas
-      </Heading>
-      <img src="/llama-sitting.png" alt="the cute llama mascot sitting and looking silly" />
+    <section data-componet="LandingPage">
+      <section id="left-panel">
+        <Heading kind="page" subtitle="Welcome to Polish">
+          Lacqueristas
+        </Heading>
+
+        <img src="/llama-sitting.png" alt="the cute llama mascot sitting and looking silly" />
+      </section>
+
+      <section id="right-panel">
+        <Newsletter />
+      </section>
+
       <section data-intent="call to action" style={callToActionStyle}>
         <Anchor kind="primary" href="/sign-up">
           Join us
