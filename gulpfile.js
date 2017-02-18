31d7b0374ac05e7bf40b6515881faf772cf6c35b
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-02-18 00:06:33 -0800

    Completely revamp the build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,172 @@
 /* eslint import/no-commonjs: "off", node/no-unpublished-require: "off" */
-const join = require("path").join
-const Dotenv = require("dotenv")
-const requireEnvironmentVariables = require("require-environment-variables")
+const {join} = require("path")
 const gulp = require("gulp")
 const gulpConcat = require("gulp-concat")
 const gulpMyth = require("gulp-myth")
 const gulpSize = require("gulp-size")
 const gulpGzip = require("gulp-gzip")
-// const gulpImagemin = require("gulp-imagemin")
+const gulpBabel = require("gulp-babel")
+const browserify = require("browserify")
+const babelify = require("babelify")
+const vinylSourceStream = require("vinyl-source-stream")
+const vinylBuffer = require("vinyl-buffer")
+const gulpChanged = require("gulp-changed")
 
-Dotenv.load({silent: true})
+const DESINATION = "./transpiled/"
+const GZIP = {
+  append: true,
+  threshold: true,
+  gzipOptions: {
+    level: 9,
+    memLevel: 9,
+  },
+}
 
-requireEnvironmentVariables([])
+gulp.task("components", () => {
+  const destination = join(DESINATION, "components")
 
-const STYLES = [
-  "./node_modules/font-awesome/css/font-awesome.css",
-  "./node_modules/normalize.css/normalize.css",
-  "./node_modules/skeleton-css/css/skeleton.css",
-  "./source/client/index.css"
-]
-const IMAGES = [
-  "./source/images/*.png",
-  "./source/images/*.ico"
-]
-const FONTS = [
-  "./node_modules/font-awesome/fonts/*"
-]
-const ASSETS = [
-  "./source/assets/browserconfig.xml",
-  "./source/assets/manifest.json",
-  "./source/assets/loadtestertool.xml",
-  "./source/assets/favicon.ico"
-]
-const STYLE = "index.css"
-const DESINATION = "./transpiled/client/"
+  return gulp.src([
+    "./source/components/**/*.js",
+  ])
+    .pipe(gulpChanged(destination))
+    .pipe(gulpBabel())
+    .pipe(gulpSize({showFiles: true}))
+    .pipe(gulp.dest(destination))
+})
+
+gulp.task("server", ["components"], () => {
+  const destination = join(DESINATION, "server")
+
+  return gulp.src([
+    "./source/server/**/*.js",
+  ])
+    .pipe(gulpChanged(destination))
+    .pipe(gulpBabel())
+    .pipe(gulpSize({
+      title: "server",
+      showFiles: true,
+    }))
+    .pipe(gulp.dest(destination))
+})
+
+gulp.task("client", ["styles", "images", "assets", "fonts"], () => {
+  const destination = join(DESINATION, "client")
+
+  return browserify({
+    entries: "./source/client/index.js",
+    transform: [
+      [babelify, {
+        ignore: "./source/**/test.js",
+      }],
+    ],
+  })
+    .bundle()
+    .pipe(vinylSourceStream("index.js"))
+    .pipe(vinylBuffer())
+    .pipe(gulpSize({
+      title: "client",
+      showFiles: true,
+    }))
+    .pipe(gulp.dest(destination))
+    .pipe(gulpGzip(GZIP))
+    .pipe(gulpSize({
+      title: "client.gz",
+      showFiles: true,
+    }))
+    .pipe(gulp.dest(destination))
+})
 
 gulp.task("styles", () => {
-  return gulp.src(STYLES)
-    .pipe(gulpConcat(STYLE))
+  const destination = join(DESINATION, "client")
+
+  return gulp.src([
+    "./node_modules/font-awesome/css/font-awesome.css",
+    "./node_modules/normalize.css/normalize.css",
+    "./node_modules/skeleton-css/css/skeleton.css",
+    "./source/client/index.css",
+  ])
+    .pipe(gulpChanged(destination))
+    .pipe(gulpConcat("index.css"))
     .pipe(gulpMyth())
-    .pipe(gulpSize({showFiles: true}))
-    .pipe(gulp.dest(DESINATION))
-    .pipe(gulpGzip({
-      append: true,
-      threshold: true,
-      gzipOptions: {
-        level: 9,
-        memLevel: 9
-      }
+    .pipe(gulpSize({
+      title: "styles",
+      showFiles: true,
     }))
-    .pipe(gulpSize({showFiles: true}))
-    .pipe(gulp.dest(DESINATION))
+    .pipe(gulp.dest(destination))
+    .pipe(gulpGzip(GZIP))
+    .pipe(gulpSize({
+      title: "styles.gz",
+      showFiles: true,
+    }))
+    .pipe(gulp.dest(destination))
 })
 
 gulp.task("images", () => {
-  return gulp.src(IMAGES)
-    // .pipe(gulpImagemin({verbose: true}))
-    .pipe(gulpSize({showFiles: true}))
-    .pipe(gulp.dest(DESINATION))
-    .pipe(gulpGzip({
-      append: true,
-      threshold: true,
-      gzipOptions: {
-        level: 9,
-        memLevel: 9
-      }
+  const destination = join(DESINATION, "client")
+
+  return gulp.src([
+    "./source/images/*.png",
+    "./source/images/*.ico",
+  ])
+    .pipe(gulpChanged(destination))
+    .pipe(gulpSize({
+      title: "images",
+      showFiles: true,
     }))
-    .pipe(gulpSize({showFiles: true}))
-    .pipe(gulp.dest(DESINATION))
+    .pipe(gulp.dest(destination))
+    .pipe(gulpGzip(GZIP))
+    .pipe(gulpSize({
+      title: "images.gz",
+      showFiles: true,
+    }))
+    .pipe(gulp.dest(destination))
 })
 
 gulp.task("assets", () => {
-  return gulp.src(ASSETS)
-    .pipe(gulpSize({showFiles: true}))
-    .pipe(gulp.dest(DESINATION))
-    .pipe(gulpGzip({
-      append: true,
-      threshold: true,
-      gzipOptions: {
-        level: 9,
-        memLevel: 9
-      }
+  const destination = join(DESINATION, "client")
+
+  return gulp.src([
+    "./source/assets/browserconfig.xml",
+    "./source/assets/manifest.json",
+    "./source/assets/loadtestertool.xml",
+    "./source/assets/favicon.ico",
+  ])
+    .pipe(gulpChanged(destination))
+    .pipe(gulpSize({
+      title: "assets",
+      showFiles: true,
     }))
-    .pipe(gulpSize({showFiles: true}))
-    .pipe(gulp.dest(DESINATION))
+    .pipe(gulp.dest(destination))
+    .pipe(gulpGzip(GZIP))
+    .pipe(gulpSize({
+      title: "assets.gz",
+      showFiles: true,
+    }))
+    .pipe(gulp.dest(destination))
 })
 
 gulp.task("fonts", () => {
-  return gulp.src(FONTS)
-    .pipe(gulp.dest(join(DESINATION, "fonts")))
+  const destination = join(DESINATION, "client", "fonts")
+
+  return gulp.src([
+    "./node_modules/font-awesome/fonts/**/*",
+  ])
+    .pipe(gulpChanged(destination))
+    .pipe(gulpSize({
+      title: "fonts",
+      showFiles: true,
+    }))
+    .pipe(gulp.dest(destination))
+    .pipe(gulpGzip(GZIP))
+    .pipe(gulpSize({
+      title: "fonts.gz",
+      showFiles: true,
+    }))
+    .pipe(gulp.dest(destination))
 })
 
-gulp.task("build", ["styles", "images", "assets", "fonts"])
+gulp.task("build", ["server", "client"])
+gulp.task("watch", ["server", "client"], () => {
+  gulp.watch("./source/server/**/*", ["server"])
+  gulp.watch("./source/client/**/*", ["client"])
+})
