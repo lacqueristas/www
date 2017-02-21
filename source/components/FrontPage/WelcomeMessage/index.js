41a658174f08d3eb5f901b0822ea981db505986e
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-02-21 00:29:27 -0800

    Better more informative error message.

--- source/components/FrontPage/WelcomeMessage/index.js
+++ source/components/FrontPage/WelcomeMessage/index.js
@@ -1,5 +1,7 @@
 import React, {PropTypes, PureComponent} from "react"
 
+import Anchor from "../../Anchor"
+
 export default class WelcomeMessage extends PureComponent {
   static propTypes = {name: PropTypes.string.isRequired}
 
@@ -7,7 +9,7 @@ export default class WelcomeMessage extends PureComponent {
     const {name} = this.props
 
     return <p>
-      Welcome to Lacqueristas, {name}!
+      Welcome to Lacqueristas, {name}! This will be your front page for Lacqueristas. You can view your notifications above, or your projects below. We suggest <Anchor href="/make-project">creating a new project</Anchor>.
     </p>
   }
 }
