363544638230f81e4fc1305fcb02bfc5d299abcd
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-02-21 00:28:25 -0800

    Meh, don't bother bringing in an entire image just for a loading screen.

--- source/components/clientDataRequired/LoadingScreen/index.js
+++ source/components/clientDataRequired/LoadingScreen/index.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function LoadingScreen () {
   return <section data-component="LoadingScreen">
-    <img src="/llama-sitting.png" alt="Just a simple loading screen" />
+    ...
   </section>
 }
