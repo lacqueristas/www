e4c80c38dc66ed9287ab5e1f066117d85c3870f1
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-02-25 10:07:12 -0800

    Fixed the casing. (#164)

--- source/components/SignUp/index.js
+++ source/components/SignUp/index.js
@@ -20,7 +20,7 @@ export default function SignUp () {
       <img src="/llama-standing.png" alt="the cute llama mascot standing with tongue out" />
     </section>
 
-    <Form name="SignUp" action="signup" slug="sign-up">
+    <Form name="SignUp" action="signUp" slug="signUp">
       <FormSection id="name" type="text" required label="Your name" slug="sign-up" />
       <FormSection id="email" type="email" required label="Your email" slug="sign-up" />
       <FormSection id="password" type="password" required label="A good password" slug="sign-up" />
