ac039d7e7578dad727a4714f055dbe5c344004b2
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-02-27 01:47:57 -0800

    Fleshing out even more of the project creation area

--- source/@lacqueristas/pages/MakeProject/index.js
+++ source/@lacqueristas/pages/MakeProject/index.js
@@ -1,21 +1,52 @@
 import React, {PropTypes, PureComponent} from "react"
-
+import {clientSide} from "@lacqueristas/decorators"
 import {Layout} from "@lacqueristas/ui"
 import {Heading} from "@lacqueristas/elements"
+import {Form} from "@lacqueristas/elements"
+import {FormSection} from "@lacqueristas/elements"
+import {Button} from "@lacqueristas/elements"
+import {ButtonGroup} from "@lacqueristas/elements"
 
-export default class MakeProject extends PureComponent {
+export default clientSide(class MakeProject extends PureComponent {
   static propTypes = {
     project: PropTypes.shape({
       attributes: PropTypes.shape({
-
-      })
-    })
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired,
   }
+
+  static defaultProps = {
+    project: {
+      attributes: {
+        name: "",
+        description: "",
+      },
+    },
+  }
+
   render () {
+    const {project} = this.props
+    const {attributes} = project
+    const {name} = attributes
+    const {description} = attributes
+
     return <Layout subtitle="Making a project" data-component="MakeProject">
       <Heading kind="page">
         Make a Project
       </Heading>
+
+      <Form name="SignUp" action="signUp" slug="signUp">
+        <FormSection id="name" type="text" required label="What do you call this?" slug="makeAProject" value={name} />
+        <FormSection id="email" type="textarea" required label="All the details" slug="makeAProject" defaultValue={description} />
+
+        <ButtonGroup>
+          <Button kind="primary" type="submit">
+            Create my first draft
+          </Button>
+        </ButtonGroup>
+      </Form>
     </Layout>
   }
-}
+})
