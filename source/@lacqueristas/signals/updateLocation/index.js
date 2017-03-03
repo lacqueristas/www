a8638c267945ea19a890a45b1cd3da85859dd9b4
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-03-02 23:13:10 -0800

    Promises really aren't valuable here

--- source/@lacqueristas/signals/updateLocation/index.js
+++ source/@lacqueristas/signals/updateLocation/index.js
@@ -1,12 +1,12 @@
 import urlParse from "url-parse"
-import {tapP} from "ramda-extra"
 import updateNavigation from "../updateNavigation"
 
 export default function updateLocation (href) {
   return function thunk (dispatch, getState, {history}) {
-    return Promise
-      .resolve(urlParse(href, true))
-      .then(tapP(history.push))
-      .then((location) => dispatch(updateNavigation(location)))
+    const destination = urlParse(href, true)
+
+    dispatch(updateNavigation(destination))
+
+    history.push(destination)
   }
 }
