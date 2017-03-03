c3d5c986dc40ee3a7692250d622aeca327c18411
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-03-02 23:14:04 -0800

    Now we can use fancy tools

--- source/client/redux/index.js
+++ source/client/redux/index.js
@@ -1,14 +1,14 @@
-/* eslint-disable immutable/no-mutation  */
+/* eslint-disable immutable/no-mutation, no-underscore-dangle  */
 import {createStore} from "redux"
 import {applyMiddleware} from "redux"
+import {compose} from "redux"
 import createLogger from "redux-logger"
 import thunkMiddleware from "redux-thunk"
 
-import history from "../history"
 import reaction from "../reaction"
 import initialState from "./initialState"
 
-export default function redux ({client}) {
+export default function redux ({client, history}) {
   if (process.env.NODE_ENV === "production") {
     return createStore(
       reaction,
@@ -19,10 +19,12 @@ export default function redux ({client}) {
     )
   }
 
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
   return createStore(
     reaction,
     initialState(),
-    applyMiddleware(
+    composeEnhancers(applyMiddleware(
       thunkMiddleware.withExtraArgument({
         client,
         history,
@@ -32,5 +34,5 @@ export default function redux ({client}) {
         collapsed: true,
       })
     )
-  )
+  ))
 }
