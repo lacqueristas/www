52199c4c85076a89ab7ed3347e2bcf6b3107c0f6
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-03-16 23:28:10 -0700

    Using the new components

--- source/@lacqueristas/ui/Body/index.js
+++ source/@lacqueristas/ui/Body/index.js
@@ -3,6 +3,9 @@ import cxs from "cxs"
 
 import {neutral} from "@lacqueristas/colors"
 
+import GoogleTagManager from "./GoogleTagManager"
+import JavascriptWarning from "./JavascriptWarning"
+
 const style = {
   backgroundColor: neutral,
   fontSize: 18,
@@ -15,39 +18,10 @@ export default class Body extends PureComponent {
     const {children} = this.props
 
     return <body className={cxs(style)} id="application">
-      <noscript>
-        <h1>Hello!</h1>
+      <GoogleTagManager />
 
-        <p>
-          You&apos;re visiting a application that works almost entirely via Javascript,
-          but you don&apos;t have Javascript enabled. I get why you might do that,
-          considering the nature of the modern web, but I like writing javascript
-          and it makes designing UI a much more enjoyable experience.
-        </p>
+      <JavascriptWarning />
 
-        <p>
-          I would love to spend time making the application work without
-          Javascript, but I just don&apos;t have the time or energy to do this! While
-          server-side rendering is very common and approachable the technology I
-          decided to work with is experimental and currently doesn&apos;t have any
-          integration with server-side rendering.
-        </p>
-
-        <p>
-          I&apos;m a big fan of the open web and accessability and I try very hard to
-          make Lacqueristas a home for <strong>anyone</strong> interested in
-          nail polish. You are not invisible to this team.
-          <a href="mailto:team@lacqueristas.club" title="Let me know you want a non-javascript version of lacqueristas">
-            I would love to hear from you on this issue and our email is open to
-            anyone wanting to talk about it.
-          </a>
-        </p>
-
-        <p>
-          Until then both Lita & the Lacqueristas Club hope you&apos;ll find a way
-          to join us.
-        </p>
-      </noscript>
       {children}
 
       <script async type="text/javascript" src="index.js" />
--- source/@lacqueristas/ui/Head/index.js
+++ source/@lacqueristas/ui/Head/index.js
@@ -1,5 +1,7 @@
 import React, {PropTypes, PureComponent} from "react"
 
+import GoogleTagManager from "./GoogleTagManager"
+
 export default class Head extends PureComponent {
   static propTypes = {
     title: PropTypes.string.isRequired,
@@ -21,6 +23,8 @@ export default class Head extends PureComponent {
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
       <title>{title} | {subtitle}</title>
+
+      <GoogleTagManager />
 
       <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
       <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
