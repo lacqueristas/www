b66ab282946a8512d399620aac1d6b76c6e36d33
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2017-03-16 23:27:28 -0700

    Creating some google tag manager stuff.

---
+++ source/@lacqueristas/ui/Body/GoogleTagManager/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function GoogleTagManager () {
+  return <noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GBMRT5" height="0" width="0" style="display:none;visibility:hidden" />
+  </noscript>
+}
