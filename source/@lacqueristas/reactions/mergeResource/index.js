7af0d5df41c2c2441a1a2474f93d545e077218a1
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-03-19 17:18:43 -0700

    New functions!

--- source/@lacqueristas/reactions/mergeResource/index.js
+++ source/@lacqueristas/reactions/mergeResource/index.js
@@ -1,13 +1,26 @@
+import {groupBy} from "ramda"
+import {prop} from "ramda"
+import {indexBy} from "ramda"
 import {mergeDeep} from "ramda-extra"
+import {treeify} from "ramda-extra"
 
 import asGraph from "./asGraph"
-import treeify from "./treeify"
 
-export default function mergeResource ({state, payload}) {
+const resourceTreeify = treeify([
+  groupBy(prop("type")),
+  indexBy(prop("id")),
+])
+
+export default function mergeResource ({state, payload}: {state: StateType, payload: object}): StateType {
   const {data} = payload
 
   return {
     ...state,
-    resources: asGraph(mergeDeep(state.resources, treeify([data]))),
+    resources: asGraph(
+      mergeDeep(
+        state.resources,
+        resourceTreeify([data])
+      )
+    ),
   }
 }
