d93a8a70dc8cd19f363d188712d6b1cee6909069
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-03-27 09:17:40 -0700

    So I was thinking we could really utilize a cool cache layer for pre-hydration

--- source/server/redux/index.js
+++ source/server/redux/index.js
@@ -3,6 +3,7 @@ import {identity} from "ramda"
 
 import initialState from "./initialState"
 
+// TODO: Possibly use client redux style with a cache layer to create pre-populated data.
 export default createStore(
   identity,
   initialState()
