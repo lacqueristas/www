31fde45536d569f6629b4896d61b4f6093059f2c
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-12 02:22:54 -0700

    Refactor for abstractions and better control flow

--- source/@lacqueristas/signals/refreshResources/index.js
+++ source/@lacqueristas/signals/refreshResources/index.js
@@ -1,13 +1,22 @@
-import {map} from "ramda"
+import {ok} from "httpstatuses"
+import {forEach} from "ramda"
+import {omit} from "ramda"
+import * as resources from "@lacqueristas/resources"
 
 import mergeResource from "../mergeResource"
+
+const MAPPING = {
+  accounts: "account",
+  sessions: "session",
+  projects: "project",
+}
 
 export default function refreshResources (): Function {
   return function thunk (dispatch: ReduxDispatchType, getState: GetStateType, {client}: {client: HSDKClientType}): Promise<SignalType> {
     const state = getState()
 
-    map((collection: Array<any>) => {
-      map((member: any): any => {
+    forEach((collection: Array<Promise<ResourceType>>) => {
+      forEach((member: ResourceType) => {
         const {id} = member
         const {type} = member
         const {meta} = member
@@ -15,13 +24,26 @@ export default function refreshResources (): Function {
 
         // TODO: Check for staleness instead of always refreshing
         if (id && type && version && client[type][version].show) {
-          return client[type][version]
+          client[type][version]
             .show({id})
-            .then(({data}: {data: any}): SignalType => dispatch(mergeResource(data)))
-        }
+            .then(({data, status}: {data: JSONAPIResponse, status: number}): ResourceType => {
+              const resource = resources[MAPPING[type]]
 
-        return member
+              switch (status) {
+                case ok: {
+                  return omit(["__abstraction__"], resource(data.data))
+                }
+                default: {
+                  return Promise.reject(new Error("We received an unexpected status code from the server"))
+                }
+              }
+            })
+            .then((resource: ResourceType): SignalType => dispatch(mergeResource(resource)))
+            .catch(console.error.bind(console))
+        }
       }, collection)
     }, state.resources)
+
+    return dispatch({type: "refreshResources"})
   }
 }
