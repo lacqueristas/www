7d6dbeb2d021f8f3a8f5f5520533573727f5680b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-16 00:11:16 -0700

    clearForm wasn't correctly removing the form data

--- source/@lacqueristas/reactions/clearFormReaction/index.js
+++ source/@lacqueristas/reactions/clearFormReaction/index.js
@@ -3,6 +3,6 @@ import {mergeDeep} from "ramda-extra"
 export default function clearFormReaction ({state, payload: slug}: {state: StateType, payload: string}): StateType {
   return mergeDeep(
     state,
-    {ephemeral: {forms: {[slug]: {}}}}
+    {ephemeral: {forms: {[slug]: undefined}}}
   )
 }
