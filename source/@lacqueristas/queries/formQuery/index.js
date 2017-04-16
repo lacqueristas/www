3fd70cbe097ff98d1908947f561e337e957b2559
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-15 19:52:40 -0700

    Fixing name

--- source/@lacqueristas/queries/formQuery/index.js
+++ source/@lacqueristas/queries/formQuery/index.js
@@ -1,6 +1,6 @@
 import {path} from "ramda"
 
-export default function formSectionQuery (state: StateType, props: FormQueryPropertiesType): FormQueryStateType {
+export default function formQuery (state: StateType, props: FormQueryPropertiesType): FormQueryStateType {
   const {slug} = props
   const attributes = path(["ephemeral", "forms", slug], state)
 
