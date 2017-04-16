46e41adee82e41e68ad163660c46ceeac6145b87
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-15 19:52:49 -0700

    Gotta export to use

--- source/@lacqueristas/queries/index.js
+++ source/@lacqueristas/queries/index.js
@@ -1,3 +1,4 @@
 export {default as formSectionQuery} from "./formSectionQuery"
 export {default as formQuery} from "./formQuery"
+export {default as onlyProps} from "./onlyProps"
 export {default as query} from "./query"
