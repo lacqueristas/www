5bb40332de89f425c0f9f6f66e7e338a7e19ad2f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-16 23:20:31 -0700

    Moving to suffixed exports

--- source/@lacqueristas/resources/index.js
+++ source/@lacqueristas/resources/index.js
@@ -1,4 +1,4 @@
 export {default as account} from "./account"
 export {default as session} from "./session"
 export {default as project} from "./project"
-export {default as photograph} from "./photograph"
+export {default as photographIncomingResource} from "./photographIncomingResource"
--- source/@lacqueristas/signals/pushPhotograph/index.js
+++ source/@lacqueristas/signals/pushPhotograph/index.js
@@ -1,6 +1,6 @@
 import {created} from "httpstatuses"
 import {omit} from "ramda"
-import {photograph} from "@lacqueristas/resources"
+import {photographIncomingResource} from "@lacqueristas/resources"
 
 export default function pushProject (client: HSDKClientType): Function {
   return function pushProjectClient ({attributes, bearer}: {attributes: FreshPhotographAttributesType}): Promise<PhotographResourceType> {
@@ -22,7 +22,7 @@ export default function pushProject (client: HSDKClientType): Function {
       .then(({data, status}: {data: JSONAPIResponse, status: number}): PhotographResourceType => {
         switch (status) {
           case created: {
-            return omit(["__abstraction__"], photograph(data.data))
+            return omit(["__abstraction__"], photographIncomingResource(data.data))
           }
           default: {
             return Promise.reject(new Error("We received an unexpected status code from the server"))
