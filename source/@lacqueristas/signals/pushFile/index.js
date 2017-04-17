da49f1a753008997c7f5e2c036c04338dfeb08d2
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-04-16 23:24:40 -0700

    A quick attempt at the lumin api

---
+++ source/@lacqueristas/signals/pushFile/index.js
@@ -0,0 +1,16 @@
+import {prop} from "ramda"
+import {invoker} from "ramda"
+import {parse} from "li"
+
+export default function pushFile (image: File): Promise<FileResourceType> {
+  return fetch(
+    `${window.env.LUMIN_LOCATION}/images?lenses=original,thumbnail`,
+    {
+      method: "POST",
+      body: image,
+    }
+  )
+    .then(prop("headers"))
+    .then(invoker(1, "get")("Link"))
+    .then(parse)
+}
