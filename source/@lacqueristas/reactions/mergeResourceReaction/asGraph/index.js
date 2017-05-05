25919f47780ebadefce37fd0765444964fa1f01a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-05-05 15:26:08 -0700

    Turns out this graph stuff is bs

--- source/@lacqueristas/reactions/mergeResourceReaction/asGraph/index.js
+++
@@ -1,8 +0,0 @@
-import {map} from "ramda"
-
-import perCollection from "./perCollection"
-
-// Tree -> Graph
-export default function asGraph (tree: StateType): StateType {
-  return map(perCollection(tree), tree)
-}
--- source/@lacqueristas/reactions/mergeResourceReaction/asGraph/perCollection/index.js
+++
@@ -1,9 +0,0 @@
-import {map} from "ramda"
-
-import perMember from "./perMember"
-
-export default function perCollection (tree) {
-  return function perCollectionWithTree (collection) {
-    return map(perMember(tree), collection)
-  }
-}
