879633c5a3c24f35a9c4efd5c1cda00d327ceb8a
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-07-31 15:13:37 -0700

    For some reason we’re linting this now

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-commonjs, no-inline-comments, immutable/no-mutation, line-comment-position, no-magic-numbers, max-lines */
 module.exports = {
   parser: "babel-eslint",
   parserOptions: {ecmaFeatures: {jsx: true}},
